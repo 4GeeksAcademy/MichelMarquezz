19d9101515c620de3d447ac038747da3db850385
michelmarq1 <marquezmichel2003@gmail.com>
2025-04-09 12:56:47 +0000

    Generador

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-import "bootstrap";
-import "./style.css";
+// Nuevos arrays de pronombres, adjetivos y sustantivos
+let pronoun = ['my', 'your', 'their'];
+let adj = ['awesome', 'fast', 'cool'];
+let noun = ['cat', 'car', 'guitar'];
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+// Función para generar las combinaciones de nombres de dominio
+const generateDomains = () => {
+    let domains = [];
+    for (let p of pronoun) {
+        for (let a of adj) {
+            for (let n of noun) {
+                domains.push(`${p}${a}${n}.com`);
+            }
+        }
+    }
+    return domains;
 };
+
+// Generar y mostrar los nombres de dominio en la consola
+const domainList = generateDomains();
+console.log("Nombres de dominio generados:");
+domainList.forEach(domain => console.log(domain));
